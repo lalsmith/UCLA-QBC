c0885bcf42e3b43c1043e18f723460271e1addea
lalsmith <lalsmith@g.ucla.edu>
2016-09-11 19:08:27 -0700

    B.2
    Script for working through B.2

---
+++ B.2.R
@@ -0,0 +1,8 @@
+# B.2 Assignment
+# I add two numbers and assign the result to an new object I call a.
+a <- 2+3
+a
+# The arrow makes the assignment, it will also tell you the contents of an object
+b <- a + a
+a+a; a+b
+# The semicolon allows you to perform two actions
