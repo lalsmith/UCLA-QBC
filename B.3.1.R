eff425b22bcd850bd9553fc9d51397e745c95e33
lalsmith <lalsmith@g.ucla.edu>
2016-09-11 19:35:13 -0700

    B.3.1
    Script for working through B.3.1 exercises

---
+++ B.3.1.R
@@ -0,0 +1,19 @@
+# B.3 Data Structures
+# B.3.1 Vectors
+# A vector is a group of numbers arranged in a row or column
+Y <- c(8.3, 8.6, 10.7, 10.8, 11, 11, 11.1, 11.2, 11.3, 11.4)
+1:4
+4:1
+-1:3
+-(1:3)
+seq(from=1, to=3, by=0.2)
+seq(1, 3, by = 0.2)
+seq(1, 3, length=7)
+# You can specify unitis of the sequence, or total length of a sequence
+# You can repeat sequences
+rep (1,3)
+# repeats the first number 3 times
+rep(1:3, 2)
+# repeats the sequence two times
+rep(1:3, each = 2)
+# repeats each number in sequence two times
